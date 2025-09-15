8c05682e2886864686eb912a8492fc960d584fb9
aditsalvani43-ui <aditsalvani26@gmail.com>
2025-09-15 16:47:14 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-sky-500">Brat Vortex</h1>
+      <p className="mt-4 text-sky-400">🎉 Halo, ini udah jalan bro 🎉</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
